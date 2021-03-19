dc357254dccba118ac8483e6bff241ee5e4896be
Wally2dq <71093295+Wally2dq@users.noreply.github.com>
2021-03-19 16:18:00 -0300

    Actualizacion
    ..

--- Calculadora/Calculadora.Final/Program.cs
+++ Calculadora/Calculadora.Final/Program.cs
@@ -11,6 +11,27 @@ namespace Calculadora.Final
     {
         static void Main(string[] args)
         {
+            string Entrada1 = "";
+            string Entrada2 = "";
+            int Valor1 = 0;
+            int Valor2 = 0;
+            //bool Validacion1 = true;
+            //bool Validacion2 = true;
+            string Salida = "x" ;
+
+            Class1 F = new Class1();
+
+            do {
+                Console.WriteLine("Ingrese 2 Numeros");
+
+                F.ValidaNumero(Entrada1,ref Valor1);
+
+                F.ValidaNumero(Entrada2, ref Valor2);
+
+                Console.WriteLine("Valores "+Valor1+" "+Valor2);
+
+
+            } while ((Salida != "X") || (Salida !="x") );
         }
     }
 }
--- Calculadora/ClassLibraryCalculadora/Class1.cs
+++ Calculadora/ClassLibraryCalculadora/Class1.cs
@@ -8,5 +8,29 @@ namespace ClassLibraryCalculadora
 {
     public class Class1
     {
+        
+
+        public void ValidaNumero(string Entrada, ref int NumeroEntrada)
+        {
+            bool bandera = false;
+
+            do
+            {
+                Console.WriteLine("Ingrese valor");
+                Entrada = Console.ReadLine();
+
+                if (!int.TryParse(Entrada, out NumeroEntrada))
+                {
+
+                    Console.WriteLine("Error: Ingrese un valor numerico");
+
+                }
+                else
+                {
+                    bandera = true;
+                }
+
+            } while (bandera == false);
+        }
     }
 }
