8dca52454dee3cddc241d873c8bbd9a876bc9485
Wally2dq <71093295+Wally2dq@users.noreply.github.com>
2021-04-16 14:07:02 -0300

    Update MaquinaExpendedora.cs

--- MaquinaExpendedora.cs
+++ MaquinaExpendedora.cs
@@ -5,40 +5,80 @@ using System.Text;
 using System.Threading.Tasks;
 using Class.Expendedora;
 using Class.Validacion;
+using Class.Exepciones;
 
 namespace ProyectoDeMaquina
 {
     class Program
     {
-        static private Expendedora expen = new Expendedora();
+        static private Expendedora expen ;
         static private Validaciones validaciones = new Validaciones();
         static void Main(string[] args)
         {
-            
+            IngresoDatosExpendedora();
+            EjecuctarOpciones();
         }
 
         static string Menu() 
         {
-            return "Bienvenido sus Opciones son" +
-                "\n 0 Encender Maquina" +
+            return "\n 0 Encender Maquina" +
                 "\n 1 Latas Disponibles" +
                 "\n 2 Insertar Lata" +
                 "\n 3 Extraer Lata" +
                 "\n 4 Balance" +
-                "\n 5 Descripcion y Stock de Latas";
+                "\n 5 Descripcion y Stock de Latas" +
+                "\n 6 Salir";
         }
+
+        static void IngresoDatosExpendedora() 
+        {
+            string Proveedor="";
+            int Cantidad =-1;
+
+            Console.WriteLine("Ingrese Proveedor");
+            validaciones.ValidarString(ref Proveedor);
+            Console.WriteLine("Ingrese la cantidad maxima");
+            validaciones.ValidarNumeroEntero(ref Cantidad);
+
+            new Expendedora(Proveedor,Cantidad);
+;        }
 
         static void EjecuctarOpciones() 
         {
-            string Ingreso;
             int Opciones=-1;
+            bool flag = false;
 
-            Menu();
-            Ingreso = Console.ReadLine();
+            do
+            {
 
-            validaciones.ValidarNumero(Ingreso,Opciones);
+                Menu();
+                Console.WriteLine("Ingrese su Opcion");
+                validaciones.ValidarNumeroEntero(ref Opciones);
 
-            //switch (Opciones)
+                switch (Opciones)
+                {
+                    case 0:
+                        expen.EncenderMaquina();
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        flag = true;
+                        break;
+                    default:
+                        Console.WriteLine("Ingrese una opcion entre 0 a 5");
+                        break;
+                }
+
+            } while (flag!=true);
         }
         static void IngresarLata(Expendedora Ex) { }
 
